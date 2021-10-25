7a1bb7d45e21880082f4813c63bd2203f8693616
ccc1cic <92039671+ccc1cic@users.noreply.github.com>
2021-10-25 12:05:26 +0300

    Add files via upload

---
+++ VCPlayBot/modules/pmpermit.py
@@ -0,0 +1,69 @@
+
+from pyrogram import Client
+import asyncio
+from VCPlayBot.config import SUDO_USERS
+from VCPlayBot.config import PMPERMIT
+from pyrogram import filters
+from pyrogram.types import Message
+from VCPlayBot.services.callsmusic import client as USER
+
+PMSET =True
+pchats = []
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+    if PMPERMIT == "ENABLE":
+        if PMSET:
+            chat_id = message.chat.id
+            if chat_id in pchats:
+                return
+            await USER.send_message(
+                message.chat.id,
+                "مرحبًا ، هذه خدمة مساعد موسيقى. \n\n ❗️ القواعد: \n- غير مسموح بالمحادثة \n- غير مسموح بالبريد العشوائي \n\n 👉 ** أرسل رابط دعوة المجموعة أو اسم المستخدم إذا لم يتمكن المستخدم من الانضمام إلى مجموعتك  . ** \n\n ⚠️ إخلاء المسؤولية: إذا كنت ترسل رسالة هنا ، فهذا يعني أن المسؤول سيرى رسالتك وينضم إلى الدردشة \n - لا تقم بإضافة هذا المستخدم إلى مجموعات سرية. \n - لا تشارك معلومات خاصة هنا  \n\n",
+            )
+            return
+
+    
+
+@Client.on_message(filters.command(["/pmpermit"]))
+async def bye(client: Client, message: Message):
+    if message.from_user.id in SUDO_USERS:
+        global PMSET
+        text = message.text.split(" ", 1)
+        queryy = text[1]
+        if queryy == "on":
+            PMSET = True
+            await message.reply_text("Pmpermit turned on")
+            return
+        if queryy == "off":
+            PMSET = None
+            await message.reply_text("Pmpermit turned off")
+            return
+
+@USER.on_message(filters.text & filters.private & filters.me)        
+async def autopmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("Approoved to PM due to outgoing messages")
+        return
+    message.continue_propagation()    
+    
+@USER.on_message(filters.command("a", [".", ""]) & filters.me & filters.private)
+async def pmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("Approoved to PM")
+        return
+    message.continue_propagation()    
+    
+
+@USER.on_message(filters.command("da", [".", ""]) & filters.me & filters.private)
+async def rmpmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if chat_id in pchats:
+        pchats.remove(chat_id)
+        await message.reply_text("Dispprooved to PM")
+        return
+    message.continue_propagation()
---
+++ VCPlayBot/modules/private.py
@@ -0,0 +1,178 @@
+
+import logging
+from time import time
+from datetime import datetime
+from VCPlayBot.config import BOT_USERNAME, BOT_NAME, ASSISTANT_NAME, OWNER_NAME, UPDATES_CHANNEL, SUPPORT_GROUP
+from VCPlayBot.helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
+from VCPlayBot.helpers.decorators import sudo_users_only
+
+logging.basicConfig(level=logging.INFO)
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>- **اهلين  {message.from_user.first_name}** \n
+- ان **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) هو بوت تشغيل اغاني او صوتيات بالمحادثات الصوتيه والمرئيه **
+
+- **سوف تجد طريقة الاستخدام في قسم » الاوامر  !**
+
+❓ **𝗙𝗼𝗿 𝗶𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝗼𝗻 𝗮𝗯𝗼𝘂𝘁 𝗮𝗹𝗹 𝗳𝗲𝗮𝘁𝘂𝗿𝗲 𝗼𝗳 𝘁𝗵𝗶𝘀 𝗯𝗼𝘁, 𝗷𝘂𝘀𝘁 𝘁𝘆𝗽𝗲 /help**
+</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "➕ Add me to your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                ],[
+                    InlineKeyboardButton(
+                        "❓ How to use Me", callback_data="cbhowtouse")
+                ],[
+                    InlineKeyboardButton(
+                         "الاوامر", callback_data="cbcmds"
+                    ),
+                    InlineKeyboardButton(
+                        "قناة المطور", url=f"https://t.me/NvvvM")
+                ],[
+                    InlineKeyboardButton(
+                        "قناة التحديثات", url=f"https://t.me/{SUPPORT_GROUP}"
+                    ),
+                    InlineKeyboardButton(
+                        "قناة البوت", url=f"https://t.me/{UPDATES_CHANNEL}")
+                ],[
+                    InlineKeyboardButton(
+                        "مطور البوت 🎖", url="https://t.me/C1CIC"
+                    )
+                ]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        f"""✅ **bot is running**\n<b>💠 **uptime:**</b> `{uptime}`""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✨ Group", url=f"https://t.me/{SUPPORT_GROUP}"
+                    ),
+                    InlineKeyboardButton(
+                        "📣 Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    )
+                ]
+            ]
+        )
+    )
+
+@Client.on_message(command(["help", f"help@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>👋🏻 **Hello** {message.from_user.mention()}</b>
+
+**Please press the button below to read the explanation and see the list of available commands !**
+
+⚡ __Powered by {BOT_NAME} A.I""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="❔ HOW TO USE ME", callback_data="cbguide"
+                    )
+                ]
+            ]
+        ),
+    )
+
+@Client.on_message(command(["help", f"help@{BOT_USERNAME}"]) & filters.private & ~filters.edited)
+async def help_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>- اهلين {message.from_user.mention} من خلال الازرار اسفل القائمة بتقدر تشوف كل الاوامر مع الشوح الخاص فيها ، كل شي سهل يا عزيزي اقراء الاوامر زين وبتفهم كل شي ♥️ !</b>
+
+
+⚡ __Powered by 𝗔 𝗟 𝗢 𝗡 𝗘 __""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "الاوامر الاساسيه", callback_data="cbbasic"
+                    ),
+                    InlineKeyboardButton(
+                        "الاوامر المتقدمه", callback_data="cbadvanced"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "اوامر الادمنيه", callback_data="cbadmin"
+                    ),
+                    InlineKeyboardButton(
+                        "اوامر المطورين", callback_data="cbsudo"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "اوامر الاونر", callback_data="cbowner"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "", callback_data="cbfun"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, message: Message):
+    start = time()
+    m_reply = await message.reply_text("جاري الحساب...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        " `سرعة البوت !`\n"
+        f"⚡️ `{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 حالة البرت:\n"
+        f"• **مدة التشغيل:** `{uptime}`\n"
+        f"• **بداية التشفيل:** `{START_TIME_ISO}`"
+    )
